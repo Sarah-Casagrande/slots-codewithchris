9ec07c6b6c135c32d0626b4a0cbacc6d486c1ca5
Sarah Casagrande <sarah.e.casagrande@gmail.com>
2022-07-10 15:03:34 -0400

    Initial Commit

---
+++ Slots/SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsApp.swift
+//  Slots
+//
+//  Created by Sarah Casagrande on 7/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
